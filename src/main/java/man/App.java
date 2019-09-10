c2c4a469eb3100a3d2cfea97963ba76e02592bb0
guillaume_collet <lomig76@yahoo.fr>
2019-09-10 14:52:20 +0200

    TP Maven - Premier commit

---
+++ src/main/java/man/App.java
@@ -0,0 +1,12 @@
+package man;
+
+/**
+ * Hello world!
+ * Commentaires blabla !
+ * Aucun intéret... juste pour le TP MAN Maven !
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
